f9d160807a96d5f6a73fee2904e619162dae5832
Enrico Gallus <enrico.gallus@gmail.com>
2022-07-06 20:06:21 +0900

    - change xml-editor label - enhance linux deb package configuration

--- src/editor/EditInXmlFormat.tsx
+++ src/editor/EditInXmlFormat.tsx
@@ -65,7 +65,7 @@ const EditInXmlFormat = (props: EditorProps) => {
                     },
                     {
                         action: Util.duplicateElement,
-                        caption: 'Copy <node />',
+                        caption: 'Duplicate <node />',
                     },
                     {
                         action: Util.moveElementUp,
