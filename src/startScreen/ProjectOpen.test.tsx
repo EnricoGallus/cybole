b2ebf2555fa0ef22469ffae7611b9f104e91bd2e
Enrico Gallus <enrico.gallus@gmail.com>
2022-07-22 09:42:27 +0900

    tauri
    - fix issues with removed mocks.ts
    - correct build argument in release build script

--- src/startScreen/ProjectOpen.test.tsx
+++ src/startScreen/ProjectOpen.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import ProjectOpen from './ProjectOpen';
-import { registerElectron } from '../tests/mocks';
 
 describe('ProjectOpen', () => {
     test('renders component', () => {
@@ -12,20 +11,11 @@ describe('ProjectOpen', () => {
     });
 
     test('openDirectoryDialog should set selected directory to input', async () => {
-        const electronMock = registerElectron();
         render(<ProjectOpen />, { wrapper: MemoryRouter });
 
         const input = screen.getByLabelText('project-directory-input');
 
         fireEvent.click(input);
-
-        await waitFor(() =>
-            expect(electronMock.openDialog).toBeCalledWith('showOpenDialog', {
-                title: 'Select the Directory of the project',
-                buttonLabel: 'Select Directory',
-                properties: ['openDirectory'],
-            })
-        );
 
         await waitFor(() => expect(input).toHaveValue('directory'));
     });
