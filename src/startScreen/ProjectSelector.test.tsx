e8c4a03554506c721323f4d35d4b69b2d5ee6b39
Enrico Gallus <enrico.gallus@gmail.com>
2022-07-22 09:42:27 +0900

    tauri
    - fix issues with removed mocks.ts
    - correct build argument in release build script

--- src/startScreen/ProjectSelector.test.tsx
+++ src/startScreen/ProjectSelector.test.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import ProjectSelector from './ProjectSelector';
-import { registerElectron } from '../mocks';
 
 describe('ProjectSelector', () => {
     test('renders component', () => {
-        const electronMock = registerElectron();
         const { container } = render(<ProjectSelector children={<div data-testid="project-open" />} />, {
             wrapper: MemoryRouter,
         });
 
         expect(container).toMatchSnapshot();
-        expect(electronMock.getAppDescription).toBeCalled();
     });
 });
