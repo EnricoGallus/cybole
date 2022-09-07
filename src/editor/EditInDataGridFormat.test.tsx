7244b938e08b60e99abde98a40cdf2d3a9522f3d
enrico <enrico.gallus@gmail.com>
2022-09-08 07:01:07 +0900

    rebased on dev

--- src/editor/EditInDataGridFormat.test.tsx
+++ src/editor/EditInDataGridFormat.test.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import EditInDataGridFormat from './EditInDataGridFormat';
-import { registerElectron } from '../mocks';
 
 describe('EditInDataGridFormat', () => {
     test('renders component', () => {
-        registerElectron();
         const { container } = render(
             <EditInDataGridFormat
                 fileKey="test.xml"
